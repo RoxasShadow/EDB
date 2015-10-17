1631366ee909c4ad5a847e6a217a4d537dc30723
RoxasShadow <webmaster@giovannicapuano.net>
2015-10-17 16:03:59 +0200

    DBMS: Add test for MySQL and PostgreSQL

---
+++ specs/dbms/postgresql_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../spec_helper'
+
+describe EDB::DBMS::PostgreSQL do
+  describe '#backup' do
+    let(:dump)    { "PGPASSWORD='password' /Applications/Postgres.app/Contents/Versions/9.3/bin/pg_dump -h localhost -p 5432 -U username -F c -b -f './sample_database.sql' sample_database" }
+    let(:cluster) { "PGPASSWORD='password' /Applications/Postgres.app/Contents/Versions/9.3/bin/pg_dumpall -h localhost -p 5432 -U username -f './cluster.sql'" }
+
+    it 'calls pg_dump correctly' do
+      allow(Kernel).to receive(:system).and_wrap_original do |m, *args|
+        expect(args[0]).to satisfy { |c| [dump, cluster].include?(c) }
+      end
+
+      EDB::DBMS::PostgreSQL.backup('.')
+    end
+
+    it 'returns the the files that have been created' do
+      allow(Kernel).to receive(:system).and_wrap_original {}
+
+      results = EDB::DBMS::PostgreSQL.backup('.')
+      expect(results).to eq ['./sample_database.sql', './cluster.sql']
+    end
+  end
+end
--- specs/dbms_spec.rb
+++ specs/dbms_spec.rb
@@ -1,16 +1,16 @@
 require_relative 'spec_helper'
 
-describe EDB::Storage do
+describe EDB::DBMS do
   describe '#supports?' do
-    context 'given storage device is supported' do
+    context 'given DBMS device is supported' do
       it 'returns true' do
-        expect(EDB::Storage.supports?('S3')).to be_truthy
+        expect(EDB::DBMS.supports?('MySQL')).to be_truthy
       end
     end
 
-    context 'given storage device is not supported' do
+    context 'given DBMS device is not supported' do
       it 'returns false' do
-        expect(EDB::Storage.supports?('CappellaAmbrosiana')).to be_falsy
+        expect(EDB::DBMS.supports?('CongoDB')).to be_falsy
       end
     end
   end
